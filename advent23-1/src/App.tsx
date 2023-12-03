dedf67b1fadfacae0f17aa09b2eab5dd138e6c66
Rigel Di Scala <zedr@zedr.com>
2023-12-03 15:58:00 +0100

    Implement exercise 1

--- advent23-1/src/App.tsx
+++ advent23-1/src/App.tsx
@@ -3,8 +3,30 @@ import {useState} from 'react'
 import './App.css'
 import fileUrl from './assets/1.txt'
 
+function charCode(s: string): number {
+    return s.charCodeAt(0);
+}
+
+const zeroCharCode = charCode('0');
+const nineCharCode = charCode('9');
+const lineFeedCode = charCode('\n');
+
 function process(text: string): string {
-    return text;
+    let acc: number = 0;
+    const stack: Array<number> = [];
+
+    for (const ch: string of text) {
+        const chCode: number = charCode(ch);
+
+        if (chCode >= zeroCharCode && chCode <= nineCharCode) {
+            stack.push(chCode - zeroCharCode)
+        } else if (chCode === lineFeedCode) {
+            acc += stack[0] * 10 + stack.pop();
+            stack.length = 0;
+        }
+    }
+
+    return acc.toString();
 }
 
 function FileText() {
